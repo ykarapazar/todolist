1ccd78d869dd8599ec65342bf1cc6bb5d7fb3bb8
ykarapazar <ykarapazar@gmail.com>
2023-01-15 13:18:36 +0300

    Bitti gibi addler

--- project0/script.js
+++ project0/script.js
@@ -8,7 +8,17 @@ const classNames = {
 const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
+let input = document.getElementById("todo-input")
+
 
 function newTodo() {
   alert('New TODO button clicked!')
+  alert(input.value)
+  var li = document.createElement("li");
+  li.appendChild(document.createTextNode(input.value))
+  console.log = (li.textContent)
+  list.appendChild(li);
+  var removebutton = document.createElement("button");
+  li.appendChild(document.createElement(removebutton));
+  //list.appendChild(inputtext)
 }
